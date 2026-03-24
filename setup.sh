258cd4672990d8dedaa74d51ef0c61c66cf6cd97
HyeongminLee <candy_carry1@naver.com>
2026-03-25 04:12:11 +0900

    change

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-pip install tensorrt==10.9.0.34 --break-system-packages
-pip install gdown --break-system-packages
+pip install tensorrt==10.9.0.34
+pip install gdown
 echo 'export LD_LIBRARY_PATH=/venv/main/lib/python3.12/site-packages/tensorrt_libs:$LD_LIBRARY_PATH' >> ~/.bashrc
 export LD_LIBRARY_PATH=/venv/main/lib/python3.12/site-packages/tensorrt_libs:$LD_LIBRARY_PATH
-apt-get install -y libzip4 socat
+apt-get install -y libzip4 socat unzip
 mkdir -p /root/katago && cd /root/katago
 wget -q https://github.com/lightvector/KataGo/releases/download/v1.16.4/katago-v1.16.4-trt10.9.0-cuda12.8-linux-x64.zip
-unzip -q katago-v1.16.4-trt10.9.0-cuda12.8-linux-x64.zip
+unzip -o -q katago-v1.16.4-trt10.9.0-cuda12.8-linux-x64.zip
 ./katago --appimage-extract
 wget -q https://media.katagotraining.org/uploaded/networks/models/kata1/kata1-b28c512nbt-s12674021632-d5782420041.bin.gz -O /root/katago/model.bin.gz
 gdown "https://drive.google.com/uc?id=132Y7pFwrOY1Hkpmpu6QsQe2KvRww6s_I" -O /root/katago/zhizi_b28_muonfd2.bin.gz
